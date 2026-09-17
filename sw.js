36cbfd0746e954580f295ef326357199f715b1b7
trunghaippfgl <trunghaippfgl@gmail.com>
2026-09-17 15:42:57 +0700

    Add files via upload

---
+++ sw.js
@@ -0,0 +1,4 @@
+const CACHE='so-tap-hoa-v001';
+const ASSETS=['./','./index.html','./manifest.webmanifest','./icon-192.png','./icon-512.png'];
+self.addEventListener('install',e=>e.waitUntil(caches.open(CACHE).then(c=>c.addAll(ASSETS))));
+self.addEventListener('fetch',e=>e.respondWith(caches.match(e.request).then(r=>r||fetch(e.request))));
